2caba42dcd4fb1d4a9a5e23d0339168e687e208f
Missael Padilla <missael.padilla@sparktechs.com>
2019-01-28 12:14:12 -0800

    SetUserCurrent

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,7 +1,8 @@
 //Register User
 import axios from 'axios';
-import { GET_ERRORS } from './types'
-
+import { GET_ERRORS,SET_CURRENT_USER } from './types'
+import setAuthToken from '../utils/setAuhtToken'
+import jwt_decode from 'jwt-decode';
 
 export const registerUser = (userData, history)  => dispatch => {
     axios
@@ -15,5 +16,36 @@ export const registerUser = (userData, history)  => dispatch => {
     );
   };
 
+
+  //Login - Get User Token
+  export const loginUser = userData => dispatch => {
+      axios.post('/api/users/login', userData)
+      .then( res => {
+            // save to local storage
+            const { token } = res.data;
+            //set token to ls
+            localStorage.setItem('jwtToken',token);
+            //set token to auth header
+            setAuthToken(token);
+            //Decode token to get user data
+            const decoded = jwt_decode(token);
+            //set current user
+            dispatch(setCurrentUser(decode));    
+
+      })
+      .catch(err => 
+        dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+    }));
+  };
+
+  //set logged in user
+  export const setCurrentUser = (decoded) => {
+return{
+    type: SET_CURRENT_USER,
+    payload: decoded
+}
+  }
 
   
